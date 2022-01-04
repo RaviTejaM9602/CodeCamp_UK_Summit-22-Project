fbbc2f5a6a405384ffca54e47891f06f25bbd0bd
Ravi Teja <72485137+RaviTejaM9602@users.noreply.github.com>
2022-01-04 17:37:18 +0000

    Added structure of speakers to js file

---
+++ speaker.js
@@ -0,0 +1,37 @@
+const speakers = [{
+  name: 'Yochai Benkler',
+  photo: './Images/Speaker_Images/speaker_01.png',
+  job: 'Berkman professor of Entrepreneurial legal Studies at Harvard Law School',
+  desc: 'Benkler studies commons-based peer production, and published his seminal book The Wealth of Networks in 2006.',
+},
+{
+  name: 'SohYeong Noh',
+  photo: './Images/Speaker_Images/speaker_02.png',
+  job: 'Director of Art Center Nabl and a board member of A.G.S Korea',
+  desc: 'As the man venue for new media art production in Korea,Nabi promotes cross-discipinary collaboration and understanding among science technology, humanities, and the arts.',
+},
+{
+  name: 'Lila Tretikov',
+  photo: './Images/Speaker_Images/speaker_03.png',
+  job: 'Executive Director of the Wikimedia Foundation',
+  desc: 'Lila Tretikov is the Executive Director of the Wikimedia Foundation, the nonprofit organization that operates Wikipedia. Wikipedia is freely available in 290 languages and used by nearly half a bilion people around the world every month',
+},
+{
+  name: 'Kilnam Chon',
+  photo: './Images/Speaker_Images/speaker_04.png',
+  job: '',
+  desc: 'Kilnam Chon helped bring the internet to Asia and is an outspoken advocate for the open web and digital com-mons In 2012, he was inducted into the inaugural class of the Internet Societys (ISOC) Internet Hall of Fame.',
+},
+{
+  name: 'Julia Leda',
+  photo: './Images/Speaker_Images/speaker_05.png',
+  job: 'President of Young Pirates of Europe',
+  desc: 'European ingetration, political democracy and participation  of youth through online as her major condern,Redas report  outlining potential changes to EU copyright law was approved by the Parlament in July.',
+},
+{
+  name: 'Ryan Merkley',
+  photo: './Images/Speaker_Images/speaker_06.png"',
+  job: 'CEO of Creative Commons, ex COO of the Mozilla Foundation',
+  desc: 'Ryan had been leading open-source projects at the Mozilla Foundation such as the open source move-ment.',
+},
+];
